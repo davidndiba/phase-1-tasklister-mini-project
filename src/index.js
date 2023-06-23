079954db358bc6dda8456d2b1ca8397f64252592
David <david.ndiba@student.moringaschool.com>
2023-06-23 20:37:01 +0300

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+const form =document.querySelector('form');
+const input = document.getElementById('new-task-description');
+const submit = form[1];
+const tasks = document.getElementById('tasks')
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    handleSubmission();
+    form.reset()
+  })
+
+})
+
+
+function handleSubmission(){  
+
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+
+  
+  btn.addEventListener('click',handleDelete)
+
+  
+  li.textContent = input.value;
+  btn.textContent = ' x';
+  btn.style.padding= '10px';
+  btn.style.margin= '20px';
+  btn.style.fontSize= '20px';
+
+  tasks.appendChild(li);
+  li.appendChild(btn);
+ }
+
+ function handleDelete(e){
+  return e.target.parentNode.remove()
+}
